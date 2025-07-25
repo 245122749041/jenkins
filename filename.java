d732e94252b46f23def596adf6a2df86f6e0a4ac
ananya <245122749041@mvsrec.edu.in>
2025-07-25 12:30:50 +0530

    Add files via upload

---
+++ filename.java
@@ -0,0 +1,14 @@
+public class filename
+{
+public static void main(String[] args)
+{
+sum=0;
+for(int i=1;i<11;i++)
+{
+ sum=sum+i;
+ }
+ System.out.println("sum="+sum);
+ double average=sum/10.0;
+ System.out.println("average="+average);
+ }
+}
